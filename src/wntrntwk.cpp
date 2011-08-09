83230bebdb9e3eb16b9d11471d3fb05b4f1c8704
Jacky Alcine <jackyalcine@gmail.com>
2011-08-09 05:38:43 -0400

    ** Updated CMake information.

--- src/wntrntwk.cpp
+++ src/wntrntwk.cpp
@@ -1,8 +1,6 @@
-/*
- *  Author: Jacky Alcine <jacky.alcine@thesii.org>
- *
- *  Copyright 2011 (c) Wintermute Developers
- *
+/**
+ * @author Jacky Alcine <jacky.alcine@thesii.org>
+ * @legalese
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
  * License as published by the Free Software Foundation; either
@@ -17,6 +15,7 @@
  * License along with this library; if not, write to the
  * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
+ * @endlegalese
  *
  */
 
